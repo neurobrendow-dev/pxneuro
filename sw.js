80075ecdebbf7f55ecf1e2345f1e67e169884e25
neurobrendow-dev <neuro.brendow@gmail.com>
2026-08-25 15:57:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Receituário de Bolso: offline. Trocar VERSAO a cada atualização do index.html. */
-var VERSAO = 'receituario-20260825-0154';
+var VERSAO = 'receituario-20260825-1540';
 var NUCLEO = ['./', './index.html', './manifest.webmanifest',
               './icon-192.png', './icon-512.png', './icon-180.png'];
 
